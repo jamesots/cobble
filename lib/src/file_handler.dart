3180c3756129af4dfc383c855e8a0c6a126c55a3
James Ots <code@jamesots.com>
2014-04-05 11:59:42 +0100

    Lookup mime types in the file handler

--- lib/src/file_handler.dart
+++ lib/src/file_handler.dart
@@ -1,8 +1,11 @@
 part of webserver;
 
+typedef String MimeResolverMethod(File file);
+
 class FileHandler implements RequestHandler {
   String _path;
   String _trim;
+
   FileHandler(String path, {String trim}) {
     _path = path;
     _trim = trim;
@@ -10,48 +13,80 @@ class FileHandler implements RequestHandler {
   
   RequestHandlerMethod _notFoundHandler;
   RequestHandlerMethod _forbiddenHandler;
+  MimeResolverMethod _mimeResolver;
   
   RequestHandlerMethod get notFoundHandler => _notFoundHandler;
                         set notFoundHandler(var value) => _notFoundHandler = value;
   
   RequestHandlerMethod get forbiddenHandler => _forbiddenHandler;
                         set forbiddenHandler(var value) => _forbiddenHandler = value;
-  
-  onRequest(HttpRequest request, HttpResponse response) {
+
+  MimeResolverMethod get mimeResolver => _mimeResolver;
+                      set mimeResolver(var value) => _mimeResolver = value;
+
+  _fileDoesNotExist(String newPath, HttpRequest request, HttpResponse response) {
+    print("File doesn't exist: ${newPath}");
+    if (_notFoundHandler != null) {
+      _notFoundHandler(request, response);
+    } else {
+      response.statusCode = HttpStatus.NOT_FOUND;
+      response.write("Not found!");
+      response.close();
+    }
+  }
+
+  _fileOutsideOfDirectory(File file, HttpRequest request, HttpResponse response) {
+    print("Trying to load file outsite of file directory: ${file.absolute.path}");
+    if (_forbiddenHandler != null) {
+      _forbiddenHandler(request, response);
+    } else {
+      response.statusCode = HttpStatus.FORBIDDEN;
+      response.write("Go away!");
+      response.close();
+    }
+  }
+
+  String _getMimeType(File file) {
+    if (_mimeResolver != null) {
+      var mimeType = _mimeResolver(file);
+      if (mimeType != null) {
+        return mimeType;
+      }
+    }
+    return mime.lookupMimeType(file.path);
+  }
+
+  _sendFile(HttpResponse response, File file) {
+    response.statusCode = HttpStatus.OK;
+
+    response.headers.set(HttpHeaders.CONTENT_TYPE, _getMimeType(file));
+    response.addStream(file.openRead()).then((_) {
+      response.close();
+    });
+  }
+
+  String _getRealPath(HttpRequest request) {
     var path = request.uri.path;
     if (_trim != null && path.startsWith(_trim)) {
       path = path.substring(_trim.length);
     }
     String newPath = "${_path}${path}";
+    return newPath;
+  }
+
+  onRequest(HttpRequest request, HttpResponse response) {
+    var newPath = _getRealPath(request);
     File file = new File(newPath);
     if (!file.existsSync()) {
-      print("File doesn't exist: ${newPath}");
-      if (_notFoundHandler != null) {
-        _notFoundHandler(request, response);
-      } else {
-        response.statusCode = HttpStatus.NOT_FOUND;
-        response.write("Not found!");
-        response.close();
-      }
+      _fileDoesNotExist(newPath, request, response);
       return;
     }
     String filePath = file.absolute.path;
     
     if (!filePath.startsWith(_path)) {
-      print("Trying to load file outsite of file directory: ${file.absolute.path}");
-      if (_forbiddenHandler != null) {
-        _forbiddenHandler(request, response);
-      } else {
-        response.statusCode = HttpStatus.FORBIDDEN;
-        response.write("Go away!");
-        response.close();
-      }
+      _fileOutsideOfDirectory(file, request, response);
     } else {
-      response.statusCode = HttpStatus.OK;
-      
-      response.addStream(file.openRead()).then((_) {
-        response.close();
-      });
+      _sendFile(response, file);
     }
   }
 }
--- lib/webserver.dart
+++ lib/webserver.dart
@@ -2,6 +2,7 @@ library webserver;
 
 import 'dart:io';
 import 'dart:async';
+import 'package:mime/mime.dart' as mime;
 
 part 'src/server.dart';
 part 'src/file_handler.dart';
